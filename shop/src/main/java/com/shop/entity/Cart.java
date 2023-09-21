6102864829048b4f6cdfaaaa74715994d0d52ef2
hiwoogi <rmcprpwjd001@gmail.com>
2023-09-21 17:34:53 +0900

    dd

---
+++ shop/src/main/java/com/shop/entity/Cart.java
@@ -0,0 +1,25 @@
+package com.shop.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@Table(name = "cart")
+@ToString
+public class Cart {
+
+    @Id
+    @Column(name = "cart_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name="member_id")
+    private Member member;
+
+}
---
+++ shop/src/main/java/com/shop/entity/Item.java
@@ -0,0 +1,51 @@
+package com.shop.entity;
+
+
+import com.shop.constant.ItemSellStatus;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name="item")
+@Getter
+@Setter
+@ToString
+public class Item {
+
+    @Id
+    @Column(name="item_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String itemNm;
+
+    @Column(name= "price", nullable = false)
+    private  int price;
+
+    @Column(nullable = false)
+    private int stockNumber; // 재고 수량
+
+    @Lob
+    @Column(nullable = false)
+    private  String itemDetail;// 상품 상세 설명
+
+    @Enumerated(EnumType.STRING)
+    private ItemSellStatus itemSellStatus; //상품 판매 상태
+
+    @ManyToMany
+    @JoinTable(
+        name="member_item",
+            joinColumns = @JoinColumn(name = "member_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id")
+    )
+    private List<Member> member;
+
+    private LocalDateTime regTime; //등록 시간
+    private  LocalDateTime updateTime; //수정시간
+}
